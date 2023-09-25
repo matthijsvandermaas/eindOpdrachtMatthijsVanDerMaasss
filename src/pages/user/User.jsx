3b56b2594bf353a1d8608872786cece8810e9286
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-09-25 18:15:58 +0200

    inschrijf form half werkende

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import './Producer.css';
+import './User.css';
 import Inschrijf_Form from "../../components/inschrijfform/Inschrijf_Form.jsx";
 
 function inschrijf_Form(){
     return(
-        <>
+        <><div className="form-container">
+
             <form>
-            <Inschrijf_Form></Inschrijf_Form>
+                <H2>inschrijfformulier</H2>
+
+                <Inschrijf_Form></Inschrijf_Form>
             </form>
+            </div>
         </>
     );
 }
