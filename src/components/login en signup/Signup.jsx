f007a4ff963b92a7545f453b965173fbcb89915b
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-11-11 15:15:54 +0100

    Merge remote-tracking branch 'origin/branch_backend_koppelen3.1' into branch_backend_koppelen3.1

--- src/components/login en signup/Signup.jsx
+++ src/components/login en signup/Signup.jsx
@@ -1,14 +1,11 @@
-import React, {  useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function SignUp() {
-    // state voor het formulier
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-
-    // state voor functionaliteit
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
 
@@ -18,13 +15,12 @@ function SignUp() {
         toggleLoading(true);
 
         try {
-            await axios.post('http://localhost:3000/register', {
+            await axios.post('POST http://localhost:8081/users/register', {
                 email: email,
                 password: password,
             });
-
             navigate('/signIn');
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             toggleError(true);
         }
@@ -36,38 +32,33 @@ function SignUp() {
         <>
             <h1>Registreren</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="email">
-                    Emailadres:
-                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </label>
+                <label htmlFor="email">E-mailadres:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
 
-                <label htmlFor="password">
-                    Gebruikersnaam:
-                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
+                <label htmlFor="password">Wachtwoord:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-                <label htmlFor="password-field">
-                    Wachtwoord:
-                    <input
-                        type="password"
-                        id="password-field"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-                {error && <p className="error">Dit account bestaat al. Probeer een ander emailadres.</p>}
-                <button
-                    type="submit"
-                    className="form-button"
-                    disabled={loading}
-                >
+                {error && <p className="error">Oeps! De opgegeven combinatie van e-mail en wachtwoord is niet correct. Controleer je gegevens en probeer het opnieuw.</p>}
+                <button type="submit" className="form-button" disabled={loading}>
                     Registreren
                 </button>
-
             </form>
 
-            <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
+            <p>
+                Heb je al een account? Je kunt je <Link to="/signIn">hier</Link> inloggen.
+            </p>
         </>
     );
 }
